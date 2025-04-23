58ce28d7492964df9d0c2c8336049c996a53747c
tushal-pethani <pethanitushal@gmail.com>
2025-04-23 15:04:25 +0530

    initial commit

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,49 +1,68 @@
-import {useEffect} from 'react'
-import '../styles/globals.css'
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
-import {wrapper} from '../redux/store'
-import { useDispatch } from 'react-redux';
-import { getAllFunding, loadAccount, loadCrowdFundingContract, loadWeb3, subscribeCrowdFundingEvents } from '../redux/interactions';
-import { Router } from 'next/router';
-import NProgress from 'nprogress'
+import { useEffect } from "react";
+import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { wrapper } from "../redux/store";
+import { useDispatch } from "react-redux";
+import {
+  getAllFunding,
+  loadAccount,
+  loadCrowdFundingContract,
+  loadWeb3,
+  subscribeCrowdFundingEvents,
+} from "../redux/interactions";
+import { Router } from "next/router";
+import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-import { chainOrAccountChangedHandler } from '../helper/helper';
+import { chainOrAccountChangedHandler } from "../helper/helper";
 
 function MyApp({ Component, pageProps }) {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    loadBlockchain()
-  }, [])
-  
+    loadBlockchain();
+  }, []);
 
-  const loadBlockchain = async() =>{
-      const web3 = await loadWeb3(dispatch)
-      const account = await loadAccount(web3,dispatch)
-      const crowdFundingContract = await loadCrowdFundingContract(web3,dispatch)
-      await getAllFunding(crowdFundingContract,web3,dispatch)
-  }
+  const loadBlockchain = async () => {
+    const web3 = await loadWeb3(dispatch);
+    const account = await loadAccount(web3, dispatch);
+    const crowdFundingContract = await loadCrowdFundingContract(web3, dispatch);
+    await getAllFunding(crowdFundingContract, web3, dispatch);
+  };
 
-  Router.events.on("routeChangeStart",()=> NProgress.start())
-  Router.events.on("routeChangeComplete",()=> NProgress.done())
-  Router.events.on("routeChangeError",()=> NProgress.done())
-  
+  Router.events.on("routeChangeStart", () => NProgress.start());
+  Router.events.on("routeChangeComplete", () => NProgress.done());
+  Router.events.on("routeChangeError", () => NProgress.done());
+
   useEffect(() => {
-    // listen for account changes
-    window.ethereum.on('accountsChanged', chainOrAccountChangedHandler);
-    // Listen for chain change
-    window.ethereum.on('chainChanged', chainOrAccountChangedHandler);
-  }, [])
-  
-  
+    if (typeof window !== "undefined" && window.ethereum) {
+      window.ethereum.on("accountsChanged", chainOrAccountChangedHandler);
+      window.ethereum.on("chainChanged", chainOrAccountChangedHandler);
+
+      // Clean up listeners when component unmounts
+      return () => {
+        window.ethereum.removeListener(
+          "accountsChanged",
+          chainOrAccountChangedHandler
+        );
+        window.ethereum.removeListener(
+          "chainChanged",
+          chainOrAccountChangedHandler
+        );
+      };
+    } else {
+      console.warn(
+        "MetaMask is not installed or window.ethereum is undefined."
+      );
+    }
+  }, []);
+
   return (
     <>
-      <ToastContainer/>
+      <ToastContainer />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default wrapper.withRedux(MyApp)
+export default wrapper.withRedux(MyApp);
